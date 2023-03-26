9f8875ec69d8749e2bf34d60a8946092887eed67
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-26 20:56:40 +0530

    modify files

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,11 +2,34 @@
 import React from 'react';
 
 const Signup = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+
+    const signup = () => {
+
+    }
+
+
+    return (
+        <div className='container'>
+            <br/>
+            <h2>Sign Up</h2>
+            <hr/>
+            <form autoComplete='off' className='form-group' onSubmit={signup}>
+                <label htmlFor='Name' >Name</label>
+                <br/>
+                <input type='text' className='form-control' required />
+                <br/>
+                <label htmlFor='Email' >Email</label>
+                <br/>
+                <input type='email' className='form-control' required />
+                <br/>
+                <label htmlFor='Password' >Password</label>
+                <br/>
+                <input type='password' className='form-control' required />
+                <br/>
+            </form>
+        </div>
+    )
 }
 
 export default Signup
