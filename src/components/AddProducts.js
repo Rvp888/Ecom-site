b0bde5a814490f3ff11aa3eb0058a18b390f8c9f
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-26 13:41:38 +0530

    modify files

--- src/components/AddProducts.js
+++ src/components/AddProducts.js
@@ -2,11 +2,26 @@
 import React from 'react';
 
 const AddProducts = () => {
-  return (
-    <div>
-      Add products
-    </div>
-  )
+    return (
+        <div className='container'>
+            <br/>
+            <h2>ADD PRODUCTS</h2>
+            <hr/>
+            <form autoComplete='off' className='form-group' onSubmit={addProducts} >
+                <label htmlFor='product-name'>Product Name</label>
+                <br/>
+                <input type="text" className='form-control' required 
+                />
+                <label htmlFor='product-price'>Product Price</label>
+                <br/>
+                <input type="number" className='form-control' required 
+                />
+                <input type="file" onChange={productImgHandler} />
+                <br/>
+                <button className='btn btn-success btn-md' >ADD</button>
+            </form>
+        </div>
+    )
 }
 
 export default AddProducts
