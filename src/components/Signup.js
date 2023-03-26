43ad159c437529ed07be13be47f1ef231cd5a203
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-26 21:14:15 +0530

    modify files

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,9 +3,13 @@ import React from 'react';
 
 const Signup = () => {
 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
-    const signup = () => {
-
+    const signup = (e) => {
+        e.preventDefault(); 
     }
 
 
@@ -17,15 +21,18 @@ const Signup = () => {
             <form autoComplete='off' className='form-group' onSubmit={signup}>
                 <label htmlFor='Name' >Name</label>
                 <br/>
-                <input type='text' className='form-control' required />
+                <input type='text' className='form-control' required 
+                onChange={(e) => setName(e.target.value)} value={name} />
                 <br/>
                 <label htmlFor='Email' >Email</label>
                 <br/>
-                <input type='email' className='form-control' required />
+                <input type='email' className='form-control' required 
+                onChange={(e) => setEmail(e.target.value)} value={email} />
                 <br/>
                 <label htmlFor='Password' >Password</label>
                 <br/>
-                <input type='password' className='form-control' required />
+                <input type='password' className='form-control' required 
+                onChange={(e) => setPassword(e.target.value)} value={password} />
                 <br/>
                 <button type='submit' className='btn btn-success btn-md mybtn' >REGISTER</button>
             </form>
