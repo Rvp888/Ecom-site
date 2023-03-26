c01fd32af5e1d1a41463cc966386fa911d412a18
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-26 20:44:42 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddProducts from './components/AddProducts';
 import Home from './components/Home';
 import { ProductsContextProvider } from './global/ProductsContext';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 export default class App extends Component {
   render() {
@@ -13,6 +15,8 @@ export default class App extends Component {
           <Routes>
             <Route exact path="/" element={<Home />} ></Route>
             <Route path="addproducts" element={<AddProducts />} ></Route>
+            <Route path="signup" element={<Signup />} ></Route>
+            <Route path="login" element={<Login />} ></Route>
           </Routes>
         </BrowserRouter>
       </ProductsContextProvider>
