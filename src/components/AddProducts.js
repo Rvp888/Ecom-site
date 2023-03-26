e220b46ba81f272f04b28a0c966fad4710747143
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-26 14:36:22 +0530

    modify files

--- src/components/AddProducts.js
+++ src/components/AddProducts.js
@@ -8,29 +8,40 @@ const AddProducts = () => {
     const [productImg, setProductImg] = useState(null);
     const [error, setError] = useState('');
 
+    const types = ['image/png', 'image/jpeg'];
+
+    const productImgHandler = (e) => {
+        let selectedFile = e.target.files[0];
+        if (selectedFile && types.includes(selectedFile.type)){
+            setProductImg(selectedFile);
+            setError('');
+        }
+        else {
+            setProductImg(null);
+            setError('Please select a valid image type png or jpeg');
+        }
+    }
+
     const addProducts = () => {
 
     }
 
-    const productImgHandler = () => {
-        
-    }
 
     return (
         <div className='container'>
             <br/>
             <h2>ADD PRODUCTS</h2>
             <hr/>
-            <form autoComplete='off' className='form-group' onSubmit={addProducts} >
+            <form autoComplete='off' className='form-group' onSubmit={addProduct} >
                 <label htmlFor='product-name'>Product Name</label>
                 <br/>
                 <input type="text" className='form-control' required 
-                />
+                onChange={(e) => setProductName(e.target.value)} value={productName} />
                 <br/>
                 <label htmlFor='product-price'>Product Price</label>
                 <br/>
                 <input type="number" className='form-control' required 
-                />
+                onChange={(e) => setProductPrice(e.target.value)} value={productPrice} />
                 <br/>
                 <label htmlFor='product-img'>Product Image</label>
                 <br/>
