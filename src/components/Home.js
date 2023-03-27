37d0cc347ed85cce11f9bb9a2c196f35fb41b942
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-28 00:31:57 +0530

    modify file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,24 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './Navbar';
 import Products from './Products';
 import '../css/Home.css';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../config/Config';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(!user) {
+                navigate('/login', {replace: true});
+            }
+        })
+    },[])
+
     return (
         <div className='wrapper'>
             <Navbar/>
