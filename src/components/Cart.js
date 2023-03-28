c88fb375beb1eed42452f99c83855ccbe340f439
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-28 16:53:22 +0530

    modify file

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,23 @@
 
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { cartContext } from './../global/CartContext';
+import { useNavigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../config/Config';
 
 const Cart = () => {
 
-    const data = useContext(cartContext);
-    console.log(data);
+    const navigate = useNavigate();
+    const { shoppingCart, dispatch, totalPrice, totalQty } = useContext(cartContext);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(!user){
+                navigate('/login', {replace: true});
+            }
+        })
+    })
+    
 
     return (
         <div>
