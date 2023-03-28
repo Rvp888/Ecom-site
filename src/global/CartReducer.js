8fc72e4d28635d4bd33c5a80fc2d1c6d734bf1bb
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-28 10:40:29 +0530

    modify file

--- src/global/CartReducer.js
+++ src/global/CartReducer.js
@@ -14,5 +14,16 @@ export const cartReducer = (state, action) => {
                 console.log('Product is already in your cart');
                 return state;
             }
+            else {
+                product = action.product;
+                product['qty'] = 1;
+                product['TotalProductPrice'] = product.ProductPrice * product.qty;
+                updatedQty = totalQty + 1;
+                updatedPrice = totalPrice + product.ProductPrice;
+                return {
+                    shoppingCart: [product, ...shoppingCart], totalPrice: updatedPrice, totalQty: updatedQty
+                }
+            }
+            break;
     }
 }
