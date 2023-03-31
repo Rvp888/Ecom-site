d76511ce7b349b122906e0d1363bcf0db22d0200
Rohan Palankar <rohanpalankar8@gmail.com>
2023-03-31 19:40:52 +0530

    modify files

--- src/global/CartContext.js
+++ src/global/CartContext.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useReducer } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
 import { CartReducer } from './CartReducer';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth, db } from "../config/Config";
@@ -9,7 +9,29 @@ export const cartContext = createContext();
 
 export const CartContextProvider = (props) => {
 
-    const [cart, dispatch] = useReducer(CartReducer, { shoppingCart: [], totalPrice: 0, totalQty: 0 });
+    const [cartDetails, setCartDetails] = useState({});
+
+    useEffect(() => {
+        onAuthStateChanged(auth, async(user) => {
+            if (user) {
+                const cartRef = doc(db, 'Carts', user.email);
+                const docSnap = await getDoc(cartRef);
+                if (docSnap.exists()) {
+                    console.log("Document data:", docSnap.data());
+                    setCartDetails(docSnap.data())
+                    console.log('cartDetails',cartDetails);
+                    // shoppingCart = docSnap.data().shoppingCart;
+                    // totalPrice = docSnap.data().totalPrice;
+                    // totalQty = docSnap.data().totalQty;
+                  } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                  }
+            }
+        })
+    },[])
+
+    const [cart, dispatch] = useReducer(CartReducer, cartDetails?.shoppingCart?.length ? cartDetails : { shoppingCart: [], totalPrice: 0, totalQty: 0 });
 
     return(
         <cartContext.Provider value={{ ...cart, dispatch }}>
